a08e3dc0d9c3431e7b662312676eb5e273b8ec50
jason <cylon.garage@gmail.com>
2025-05-21 18:05:03 +0700

    fix historicals type error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="AlpacaLib",
-    version="0.1.6",
+    version="0.1.7",
     author="Jason",
     author_email="",
     description="Alpaca Data API",
